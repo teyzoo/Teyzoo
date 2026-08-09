3c61865bbcf142d6c7217bbad839dadb42ed6205
teyzoo <igorborisenko676@icloud.com>
2026-08-09 18:52:47 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,15 +1,3 @@
 from __future__ import annotations
 
-"""
-Telegram handlers package.
-
-Все роутеры подключаются
-в main.py.
-"""
-
-__all__ = [
-    "start",
-    "signals",
-    "applications",
-    "admin",
-]
+__all__ = []
