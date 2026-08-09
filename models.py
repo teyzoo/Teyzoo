91c9c737dc08e7a2d69f50f79375c6587651a5dc
teyzoo <igorborisenko676@icloud.com>
2026-08-09 18:12:24 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,18 +1,16 @@
 from __future__ import annotations
+
 from enum import Enum
+
+
 class Direction(str, Enum):
     UP = "UP"
     DOWN = "DOWN"
+
+
 class SignalStatus(str, Enum):
     PENDING = "PENDING"
     WON = "WON"
     LOST = "LOST"
     CANCELLED = "CANCELLED"
-class UserStatus(str, Enum):
-    ACTIVE = "ACTIVE"
-    BLOCKED = "BLOCKED"
-class ApplicationStatus(str, Enum):
-    NEW = "NEW"
-    ACCEPTED = "ACCEPTED"
-    REJECTED = "REJECTED"
-    CLOSED = "CLOSED"
+    UNKNOWN = "UNKNOWN"
